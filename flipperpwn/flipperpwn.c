69f414c787b51d6f509ba91b717cc1117f23dcf4
Ben Barker <bbarker@barkbite.org>
2026-03-09 13:01:13 -0400

    Bump FlipperPwn to v1.2 with updated about screen

--- flipperpwn/flipperpwn.c
+++ flipperpwn/flipperpwn.c
@@ -480,7 +480,7 @@ static void fpwn_main_menu_callback(void* ctx, uint32_t index) {
     case FPwnMainMenuAbout:
         widget_reset(app->about);
         widget_add_string_element(
-            app->about, 64, 2, AlignCenter, AlignTop, FontPrimary, "FlipperPwn v1.1");
+            app->about, 64, 2, AlignCenter, AlignTop, FontPrimary, "FlipperPwn v1.2");
         widget_add_string_element(
             app->about, 64, 16, AlignCenter, AlignTop, FontSecondary, "Modular Pentest Framework");
         {
@@ -496,13 +496,7 @@ static void fpwn_main_menu_callback(void* ctx, uint32_t index) {
                 app->about, 64, 28, AlignCenter, AlignTop, FontSecondary, about_info);
         }
         widget_add_string_element(
-            app->about,
-            64,
-            40,
-            AlignCenter,
-            AlignTop,
-            FontSecondary,
-            "HID+WiFi+Exfil+VAR scripting");
+            app->about, 64, 40, AlignCenter, AlignTop, FontSecondary, "HID+WiFi+Exfil+Loops+Creds");
         widget_add_string_element(
             app->about, 64, 52, AlignCenter, AlignTop, FontSecondary, "github.com/barkandbite");
         g_current_view = FPwnViewAbout;
